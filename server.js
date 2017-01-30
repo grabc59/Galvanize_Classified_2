3524f82e67206a1f2caa6a49bb8c3e3db65f5594
Christopher Grabski <grabc59@gmail.com>
2017-01-30 10:47:41 -0700

    add basic script links to index and middleware to server

--- server.js
+++ server.js
@@ -12,6 +12,24 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')))
 app.use('/classifieds',messages);
 
+app.use('*', function(req, res, next) {
+  res.sendFile('index.html', {root: path.join(__dirname, 'public')})
+})
+
+app.use(function(req, res, next) {
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+  console.log(err)
+  res.status(err.status || 500)
+  res.json(err)
+})
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
