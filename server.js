4ec2f04396230579a8c3ebaaa58c8acef97779cd
Christopher Grabski <grabc59@gmail.com>
2017-01-30 10:33:04 -0700

    added clickable console log link to server js, all tests pass from part 1

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use('/classifieds',messages);
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log('Listening on port', port);
+  console.log('Listening on http://localhost:'+ port);
 });
 
 module.exports = app;
