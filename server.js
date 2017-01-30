1a8ff4658e66c2625d5fb24b07659f0e7556cd5c
Christopher Grabski <grabc59@gmail.com>
2017-01-30 10:39:41 -0700

    require path module, add index file, server serves index successfully

--- server.js
+++ server.js
@@ -3,10 +3,13 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const path = require('path')
+
 
 const messages = require('./routes/classifieds');
-app.use(bodyParser.json());
 
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')))
 app.use('/classifieds',messages);
 
 const port = process.env.PORT || 3000;
